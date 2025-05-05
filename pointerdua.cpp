7a03ab4e9fe025a6b8afa9fd9516f5c4a910c173
MuhammadChoirulRizal <mchoirulrizal9@gmail.com>
2025-05-05 19:08:47 +0700

    pointer object mhs

--- pointerdua.cpp
+++ pointerdua.cpp
@@ -10,3 +10,12 @@ class mahasiswa // membuat kelas
         cout << "no induk =" << nim << endl;
     }
 };
+
+int main()
+{
+    mahasiswa *mhs = new mahasiswa{1};// membuat pointer object mhs
+    mhs -> nim = 2;
+    mhs -> showNim();
+    delete mhs;
+    return 0;
+}
